8109e36250a743be0a570207efc6a5e35d577e15
Amine <aminejamal003@gmail.com>
2022-09-28 23:18:56 +0300

    landing page design completed

--- app/src/main/java/com/cdev/newsapp/LandingActivity.java
+++ app/src/main/java/com/cdev/newsapp/LandingActivity.java
@@ -3,12 +3,47 @@ package com.cdev.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class LandingActivity extends AppCompatActivity {
+    String name;
+    ListView my_list;
+    Random rand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        Bundle bundle = getIntent().getExtras();
+        String name = bundle.getString("message");
+
+        rand = new Random();
+        my_list = (ListView) findViewById(R.id.my_list);
+        populate(5);
     }
+    public void populate(int x)
+    {
+        Log.d("inside", "IM INSEDESAFR");
+        ArrayList<String> random_nums = new ArrayList<String>();
+        double random_value = 0;
+        for(int i =0; i <x; i++)
+        {
+            DecimalFormat formatter = new DecimalFormat(".##");
+            random_value = rand.nextDouble();
+            random_nums.add(formatter.format(random_value));
+
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, random_nums);
+        my_list.setAdapter(adapter);
+    }
+
+
 }
--- app/src/main/java/com/cdev/newsapp/MainActivity.java
+++ app/src/main/java/com/cdev/newsapp/MainActivity.java
@@ -2,14 +2,37 @@ package com.cdev.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+     Button proceed;
+     EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        proceed = (Button) findViewById(R.id.btn_proceed);
+        name = (EditText) findViewById(R.id.inputText_name);
+
+    }
+
+    public void proceedToLanding(View v) {
+        String str_name = name.getText().toString();
+        if (str_name.equals("")){
+            Toast.makeText(this, "please enter a valid user", Toast.LENGTH_LONG);
+        } else {
+            Intent i = new Intent(this, LandingActivity.class);
+            i.putExtra("message", str_name);
+            startActivity(i);
+        }
     }
 }
