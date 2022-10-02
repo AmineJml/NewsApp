7404707d67e8282feef24d922039f723005c23e1
Amine <aminejamal003@gmail.com>
2022-10-03 02:48:35 +0300

    splash screen completed

--- app/src/main/java/com/cdev/newsapp/LandingActivity.java
+++ app/src/main/java/com/cdev/newsapp/LandingActivity.java
@@ -60,7 +60,7 @@ public class LandingActivity extends AppCompatActivity {
      // populateTest();
         populateFinal();
         //arL();
-     //   clickOnList();
+       clickOnList();
 
 
     }
@@ -102,8 +102,8 @@ public class LandingActivity extends AppCompatActivity {
 
             String test = "publishedAtTest";
             SQLiteDatabase newsdb = this.openOrCreateDatabase("newsdb", MODE_PRIVATE, null);
-            newsdb.execSQL("CREATE Table IF NOT EXISTS finaTestTable (ID INTEGER PRIMARY KEY AUTOINCREMENT,title VARCHAR, author VARCHAR, publishedAt VARCHAR, description VARCHAR)");
-            newsdb.execSQL("INSERT INTO finaTestTable(title, author, publishedAt, description) VALUES ('Magician', 'abracadabra', test,'The best magic tricks in the worls starts with abracadabra' )");
+            //newsdb.execSQL("CREATE Table IF NOT EXISTS finaTestTable (ID INTEGER PRIMARY KEY AUTOINCREMENT,title VARCHAR, author VARCHAR, publishedAt VARCHAR, description VARCHAR)");
+           // newsdb.execSQL("INSERT INTO finaTestTable(title, author, publishedAt, description) VALUES ('Magician', 'abracadabra', 'dum bada tes','The best magic tricks in the worls starts with abracadabra' )");
             // newsdb.execSQL("INSERT INTO finaTestTable(title, author, publishedAt, description) VALUES ('TUTUTU', 'train boy', ' 23 september', 'tututu is the fastest train in the world')");
             //  newsdb.execSQL("DELETE FROM finaTestTable where title = 'AA'");
             // newsdb.execSQL("DELETE FROM finaTestTable where title = 'BB'");
@@ -130,77 +130,8 @@ public class LandingActivity extends AppCompatActivity {
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, the_list);
         my_list.setAdapter(adapter);
-        Intent popUp = new Intent(this, popUpActivity.class);
-        my_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                selectedItem = (String) adapterView.getItemAtPosition(i);
-                popUp.putExtra("key",  selectedItem);
-                startActivity(popUp);
-            }
-        });
+
 
     }
-
-    public void populateTest()
-    {
-        try{
-            //how to insert item only once
-            //how to select items by name
-            //cursor reexplanation
-            my_list = findViewById(R.id.my_list);
-
-            the_list = new ArrayList<String>();
-            SQLiteDatabase newsdb = this.openOrCreateDatabase("newsdb", MODE_PRIVATE, null);
-
-            newsdb.execSQL("CREATE Table IF NOT EXISTS news (title VARCHAR, author VARCHAR)");
-            newsdb.execSQL("INSERT INTO news(title, author) VALUES ('AA', 'AA')");
-            newsdb.execSQL("INSERT INTO news(title, author) VALUES ('BB', 'BB')");
-
-            Cursor c = newsdb.rawQuery("Select * from news", null);
-
-            int title = c.getColumnIndex("title");
-            int author = c.getColumnIndex("author");
-            c.moveToFirst();
-            while(c!= null){
-                String name = c.getString(title) + " by " + c.getString(author) + " " ;
-                the_list.add(name);
-                Log.d("key4", "problem here");
-                c.moveToNext();
-            }
-        //why cant we add code here?  (adapter)
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, the_list);
-        my_list.setAdapter(adapter);
-    }
-
-    public void populate()
-    {
-        my_list = findViewById(R.id.my_list);
-        the_list = new ArrayList<String>();
-        //the_list = new ArrayList<String>(Arrays.asList("Mobile", "Web", "CP2"));
-
-        the_list.add("Mobile Computing");
-        the_list.add("CP1");
-        the_list.add("Web");
-
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, the_list);
-        //allow us to add them to the list view
-        my_list.setAdapter(adapter);
-
-        //HELP
-
-        my_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String selectedItem = (String) adapterView.getItemAtPosition(i);
-                Toast.makeText(getApplicationContext(), selectedItem, Toast.LENGTH_LONG).show();
-
-            }
-        });
-    }
-
 
 }
---
+++ app/src/main/java/com/cdev/newsapp/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.cdev.newsapp;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import javax.xml.parsers.SAXParser;
+
+//SPLASH SCREEN - PART 1
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+
+            public void run() {
+
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                }
+
+            }
+
+        };
+        thread.start();
+
+    }
+}
