939572e80c0330c1f7033adb405d831196e64468
Amine <aminejamal003@gmail.com>
2022-10-03 03:22:54 +0300

    add new article design completed

---
+++ app/src/main/java/com/cdev/newsapp/AddNewArticle.java
@@ -0,0 +1,14 @@
+package com.cdev.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_article);
+    }
+}
